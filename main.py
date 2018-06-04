76980af62a946453d49c3f62180ae63a71032a00
Yoimer <ydrf19@gmail.com>
2018-06-03 22:07:46 -0400

    downgraded cryptography, updated runtime and tested memory game from jhon wheeler

--- main.py
+++ main.py
@@ -6,15 +6,6 @@ from flask import Flask, render_template
 
 from flask_ask import Ask, statement, question, session
 
-######################################
-from flask import Flask
-
-import json
-
-import requests
-
-######################################
-
 
 app = Flask(__name__)
 
@@ -22,129 +13,122 @@ ask = Ask(app, "/")
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+
 @ask.launch
 
-def launch_app():
+def new_game():
 
     welcome_msg = render_template('welcome')
 
-    return statement(welcome_msg)
-    
-
-@ask.intent("OnIntent")
-
-def turn_on():
-
-    sess = requests.Session()
-
-    url = 'http://castillolk.com.ve/proyectos/sms/alexa.php?'
-     
-    data = sess.get(url)
-     
-    print data.content
-
-    if 'ON' in data.content:
-
-        turn_on_msg = "System is already turned on. Not action taken."
-
-        print(turn_on_msg)
-
-        return statement(turn_on_msg)
-
-    else:
-        sess = requests.Session()
-
-        # sent ON to Nodemcu by sendind 7 to db on castillolk
-        url = 'http://castillolk.com.ve/proyectos/sms/alexa.php?sw=7'
-     
-        data = sess.get(url)
-     
-        print data.content
-     
-        print "next line is the statement"
-
-        turn_on_msg = "Turning system ON... It might take a few seconds, please wait."
-        
-        print(turn_on_msg)
-        
-        return statement(turn_on_msg)
+    return question(welcome_msg)
 
 
-@ask.intent("OffIntent")
+@ask.intent("YesIntent")
 
-def turn_off():
+def next_round():
 
-    sess = requests.Session()
+    numbers = [randint(0, 9) for _ in range(3)]
 
-    url = 'http://castillolk.com.ve/proyectos/sms/alexa.php?'
-     
-    data = sess.get(url)
-     
-    print data.content
+    round_msg = render_template('round', numbers=numbers)
 
-    if 'OFF' in data.content:
+    session.attributes['numbers'] = numbers[::-1]  # reverse
 
-        turn_off_msg = "System is already turned off. Not action taken."
+    return question(round_msg)
 
-        print(turn_off_msg)
 
-        return statement(turn_off_msg)
+@ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int})
+
+def answer(first, second, third):
+
+    winning_numbers = session.attributes['numbers']
+
+    if [first, second, third] == winning_numbers:
+
+        msg = render_template('win')
 
     else:
-        sess = requests.Session()
 
-        # sent ON to Nodemcu by sendind 8 to db on castillolk
-        url = 'http://castillolk.com.ve/proyectos/sms/alexa.php?sw=8'
-     
-        data = sess.get(url)
-     
-        print data.content
-     
-        print "next line is the statement"
+        msg = render_template('lose')
 
-        turn_on_msg = "Turning system OFF... It might take a few seconds, please wait."
+    return statement(msg)
 
-        print(turn_on_msg)
-     
-        return statement(turn_on_msg)
+# if __name__ == '__main__':
 
-@ask.intent("TemperatureIntent")
-
-def get_temperature():
-    sess = requests.Session()
-
-    url = 'https://phpcourse.000webhostapp.com/temperature.txt'
-
-    data = sess.get(url)
-     
-    print data.content
-     
-    print "next line is temperature"
-    
-    temperature_msg = "Temperature value is: " + data.content + " Celsius degrees"
-
-    #return statement(data.content)
-    return statement(temperature_msg)
-
-@ask.intent("HumidityIntent")
-
-def get_humidity():
-    sess = requests.Session()
-
-    url = 'https://phpcourse.000webhostapp.com/humidity.txt'
-
-    data = sess.get(url)
-     
-    print data.content
-     
-    print "next line is humidity"
-    
-    humidity_msg = "Humidity value is: " + data.content + " Percentage"
-
-    #return statement(data.content)
-    return statement(humidity_msg)
-
+#     app.run(debug=True)
 
 if __name__ == '__main__':
 
     app.run(debug=True, host='0.0.0.0')
+
+# import logging
+
+# from random import randint
+
+# from flask import Flask, render_template
+
+# from flask_ask import Ask, statement, question, session
+
+# ######################################
+# from flask import Flask
+
+# import json
+
+# import requests
+
+# ######################################
+
+
+# app = Flask(__name__)
+
+# ask = Ask(app, "/")
+
+# logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+# @ask.launch
+
+# def launch_app():
+
+#     welcome_msg = render_template('welcome')
+
+#     return statement(welcome_msg)
+
+# @ask.intent("TemperatureIntent")
+
+# def get_temperature():
+#     sess = requests.Session()
+
+#     url = 'https://phpcourse.000webhostapp.com/temperature.txt'
+
+#     data = sess.get(url)
+     
+#     print data.content
+     
+#     print "next line is temperature"
+    
+#     temperature_msg = "Temperature value is: " + data.content + " Celsius degrees"
+
+#     #return statement(data.content)
+#     return statement(temperature_msg)
+
+# @ask.intent("HumidityIntent")
+
+# def get_humidity():
+#     sess = requests.Session()
+
+#     url = 'https://phpcourse.000webhostapp.com/humidity.txt'
+
+#     data = sess.get(url)
+     
+#     print data.content
+     
+#     print "next line is humidity"
+    
+#     humidity_msg = "Humidity value is: " + data.content + " Percentage"
+
+#     #return statement(data.content)
+#     return statement(humidity_msg)
+
+
+# if __name__ == '__main__':
+
+#     app.run(debug=True, host='0.0.0.0')
